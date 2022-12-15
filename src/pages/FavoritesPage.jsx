5a9e5491087fdd1ff24c4c5604b51ec2972cfd7b
H-Cavid <cavid.hasanov2@bk.ru>
2022-12-15 17:16:11 +0400

    <<<<----Die Aufgabe hat erledigt---->>>>

---
+++ src/pages/FavoritesPage.jsx
@@ -0,0 +1,52 @@
+import React, { useContext, useEffect } from "react";
+import { useSelector } from "react-redux";
+import FavListItem from "../components/FavListItem";
+import { selectFavs } from "../feautures/favListSlice";
+import { selectFavName } from "../feautures/listNameSlice";
+
+import Splide from "@splidejs/splide";
+
+const FavoritesPage = () => {
+  const favs = useSelector(selectFavs);
+
+  const listName = useSelector(selectFavName);
+
+  useEffect(() => {
+
+
+
+
+    var splide = new Splide( '.splide', {
+      perPage: 1,
+      rewind : true,
+    } );
+    
+    splide.mount();
+  }, []);
+
+  return (
+    <div className="container mt-5">
+      <h2>{listName}</h2>
+
+
+      <section id="image-carousel" className="splide" aria-label="Beautiful Images">
+        <div className="splide__track">
+          <ul className="splide__list">
+            {favs.map((m) => (
+              <li key={m.id} className="splide__slide">
+                <FavListItem
+                  id={m.id}
+                  title={m.title}
+                  year={m.year}
+                  poster={m.poster}
+                />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default FavoritesPage;
